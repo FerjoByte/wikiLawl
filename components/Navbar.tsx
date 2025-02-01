e2e524bc2f509af7d07499c77ae3deca9e72f96b
Ferjo <ferjobyte@gmail.com>
2025-01-31 23:39:36 -0300

    menu details

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,8 +8,8 @@ const Navbar = () => {
     }
 
     return(
-        <nav className="flex justify-between items-center h-16 lg:h-auto bg-[#232625] lg:bg-transparent lg:block">
-            <div className="flex-1 flex justify-center lg:justify-start  ">
+        <nav className="flex items-center h-16 lg:h-auto bg-[#232625] lg:bg-transparent lg:block">
+            <div className="flex-1 flex justify-center lg:justify-start p-8 ml-24">
                 <img src="/logo.png" alt="wikiLawl logo" className="w-24 h-auto"/>
             </div>
             
@@ -19,11 +19,19 @@ const Navbar = () => {
             className="w-10 h-10"/>
             </button>
 
-            <div className={`${isMenuOpen ? 'block' : 'hidden'} lg:block`}>
-                <ul>
-                    <li><a href="#">Primeros pasos</a></li>
-                    <li><a href="#">Monturas</a></li>
-                    <li><a href="#">Trajes</a></li>
+            <button onClick={toggleMenu} 
+                className={`mr-4 lg:hidden ${isMenuOpen ? 'block' : 'hidden'}`}>
+                <img src="/close-menu.png" alt="Close-menu" className="w-6 h-8"/>
+            </button>
+
+            <div className={`${isMenuOpen ? 'absolute top-14 right-0 block p-4  z-50 w-64' : 'hidden'} lg:block`}>
+                <ul className="text-center text-white lg:w-64 ml-10">
+                    <li className="bg-[#232625] border-[3px] border-[#655333] p-2 rounded-lg mb-2 lg:hover:bg-[#3a3b3c] lg:hover:border-[#8b6f4d] lg:hover:text-[1.02rem] "><a href="#">PRIMEROS PASOS</a></li>
+                    <li className="bg-[#232625] border-[3px] border-[#655333] p-2 rounded-lg mb-2 lg:hover:bg-[#3a3b3c] lg:hover:border-[#8b6f4d] lg:hover:text-[1.02rem]"><a href="#">MONTURAS</a></li>
+                    <li className="bg-[#232625] border-[3px] border-[#655333] p-2 rounded-lg mb-2 lg:hover:bg-[#3a3b3c] lg:hover:border-[#8b6f4d] lg:hover:text-[1.02rem]"><a href="#">TRAJES</a></li>
+                    <li className="bg-[#232625] border-[3px] border-[#655333] p-2 rounded-lg mb-2 lg:hover:bg-[#3a3b3c] lg:hover:border-[#8b6f4d] lg:hover:text-[1.02rem]"><a href="#">MISIONES</a></li>
+                    <li className="bg-[#232625] border-[3px] border-[#655333] p-2 rounded-lg mb-2 lg:hover:bg-[#3a3b3c] lg:hover:border-[#8b6f4d] lg:hover:text-[1.02rem]"><a href="#">SUPPORT</a></li>
+                    <li className="bg-[#232625] border-[3px] border-[#655333] p-2 rounded-lg mb-2 lg:hover:bg-[#3a3b3c] lg:hover:border-[#8b6f4d] lg:hover:text-[1.02rem]"><a href="#">CASAS</a></li>
                 </ul>
             </div>
         </nav>
